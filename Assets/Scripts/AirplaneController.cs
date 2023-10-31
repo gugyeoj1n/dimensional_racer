a93dbbc8274833b61c4c4bc6c40522ebe0b19f4b
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2023-11-01 06:40:29 +0900

    [REFACTOR] 이동 스크립트 리팩터링

--- Assets/Scripts/AirplaneController.cs
+++ Assets/Scripts/AirplaneController.cs
@@ -6,55 +6,45 @@ using UnityEngine;
 
 public class AirplaneController : MonoBehaviour
 {
-    public float rollAmount;
-    public float pitchAmount;
-    public float yawAmount;
-    public float speed;
-    public float lerpAmount;
+    [SerializeField]
+    private float rollAmount;
+    [SerializeField]
+    private float pitchAmount;
+    [SerializeField]
+    private float yawAmount;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float lerpAmount;
 
-    Vector3 rotateValue;
-    Rigidbody rb;
+    private Vector3 rotateValue;
+    private Rigidbody rb;
 
-    float pitchValue;
-    float yawValue;
-    float rollValue;
+    private float pitchValue;
+    private float yawValue;
+    private float rollValue;
 
     void MoveAircraft()
     {
-        // Vector3 lerpVector = new Vector3(pitchValue * pitchAmount, yawValue * rollAmount, rollValue * rollAmount);
-        Vector3 lerpVector = new Vector3(pitchValue * pitchAmount, yawValue * yawAmount, rollValue * rollAmount);
+        Vector3 lerpVector = new Vector3(pitchValue * pitchAmount, yawValue * yawAmount, -rollValue * rollAmount);
         rotateValue = Vector3.Lerp(rotateValue, lerpVector, lerpAmount * Time.fixedDeltaTime);
         rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateValue * Time.fixedDeltaTime));
 
         rb.velocity = transform.forward * speed * Time.fixedDeltaTime;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate() 
+    void FixedUpdate()
     {
-        if (Input.GetButton("Pitch"))
-            pitchValue = Input.GetAxisRaw("Pitch");
-        else
-            pitchValue = 0;
-
-        if (Input.GetButton("Roll"))
-            rollValue = Input.GetAxisRaw("Roll");
-        else
-            rollValue = 0;
-
-        if (Input.GetButton("Yaw"))
-            yawValue = Input.GetAxisRaw("Yaw");
-        else
-            yawValue = 0;
+        pitchValue = Input.GetAxisRaw("Pitch");
+        rollValue = Input.GetAxisRaw("Roll");
+        yawValue = Input.GetAxisRaw("Yaw");
 
         MoveAircraft();
-
-        // transform.Rotate(Vector3.up * Time.deltaTime * 3);
     }
 
 }
